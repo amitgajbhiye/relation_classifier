18bf8cc1a3b33aac669cd211cf368757fcd16e1d
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-05-04 10:49:12 +0100

    get clustered data fastetxt

--- run.sh
+++ run.sh
@@ -2,17 +2,17 @@
 
 #SBATCH --job-name=RelClass
 
-#SBATCH --output=logs/out_relation_classifier.txt
-#SBATCH --error=logs/err_relation_classifier.txt
+#SBATCH --output=logs/out_fasttext_data_relation_classifier.txt
+#SBATCH --error=logs/err_fasttext_data_relation_classifier.txt
 
 #SBATCH --tasks-per-node=5
 #SBATCH --ntasks=5
 #SBATCH -A scw1858
 
-#SBATCH -p gpu,gpu_v100
-#SBATCH --mem=12G
+#SBATCH -p gpu_v100,gpu
+#SBATCH --mem=14G
 
-#SBATCH -t 0-10:00:00
+#SBATCH -t 0-5:00:00
 
 #SBATCH --gres=gpu:1
 
