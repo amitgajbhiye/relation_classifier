50940dc266e755b827edc06790ade5dc41dfcb30
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-05-10 10:05:45 +0100

    cuda numberbatch

--- run_number.sh
+++ run_number.sh
@@ -2,19 +2,22 @@
 
 #SBATCH --job-name=numBRelClass
 
-#SBATCH --output=logs/out_numberbatch_data_relation_classifier.txt
-#SBATCH --error=logs/err_numberbatch_data_relation_classifier.txt
+#SBATCH --output=logs/out_cuda_numberbatch_data_relation_classifier.txt
+#SBATCH --error=logs/err_cuda_numberbatch_data_relation_classifier.txt
 
 #SBATCH --tasks-per-node=5
 #SBATCH --ntasks=5
 #SBATCH -A scw1858
 
 #SBATCH -p gpu_v100
-#SBATCH --mem=32G
+#SBATCH --mem=10G
 #SBATCH --exclusive
 
-#SBATCH -t 2-00:00:00
+#SBATCH -t 0-00:30:00
 #SBATCH --gres=gpu:1
+
+module load CUDA/11.7
+
 
 conda activate relbert
 
