67caf67aefbcff18fd451bf96ab95cb63d440956
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-06-12 14:27:49 +0100

    classification rel scoring wordnet domain

--- run2_relscoring_classi_wordnet_all_static_emb.sh
+++ run2_relscoring_classi_wordnet_all_static_emb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash --login
 
-#SBATCH --job-name=FastTRelClass
+#SBATCH --job-name=WordNet
 
-#SBATCH --output=logs/bablenet_domain/out_relscoring_classi_babelnet_domain_all_static_emb.txt
-#SBATCH --error=logs/bablenet_domain/err_relscoring_classi_babelnet_domain_all_static_emb.txt
+#SBATCH --output=logs/wordnet_classi/out_relscoring_classi_wordnet_classi_vocab_all_static_emb.txt
+#SBATCH --error=logs/wordnet_classi/err_relscoring_classi_wordnet_classi_vocab_all_static_emb.txt
 
 #SBATCH --tasks-per-node=5
 #SBATCH --ntasks=5
@@ -12,7 +12,7 @@
 #SBATCH -p gpu,gpu_v100
 #SBATCH --mem=20G
 
-#SBATCH -t 0-16:00:00
+#SBATCH -t 0-22:00:00
 
 #SBATCH --gres=gpu:2
 
@@ -20,6 +20,6 @@ source venv_relbert/bin/activate
 
 module load CUDA/11.7
 
-python3 importance_classifier_production.py /scratch/c.scmag3/commonality_detection/output_files/classification_vocabs_similar_thresh_50/bablenet_domain
+python3 importance_classifier_production.py /scratch/c.scmag3/commonality_detection/output_files/classification_vocabs_similar_thresh_50/wordnet
 
 echo 'Job Finished !!!'
