7792262b40bf9309a0515355acb3a5986a590c96
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-06-12 16:21:50 +0100

    ontology completion wine vocab

---
+++ run8_relscoring_onto_wine_all_static_emb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=wine
+
+#SBATCH --output=logs/ontology_completion/out_relscoring_onto_wine_vocab_all_static_emb.txt
+#SBATCH --error=logs/ontology_completion/err_relscoring_onto_wine_vocab_all_static_emb.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu,gpu_v100
+#SBATCH --mem=20G
+
+#SBATCH -t 0-10:00:00
+
+#SBATCH --gres=gpu:2
+
+source venv_relbert/bin/activate
+
+module load CUDA/11.7
+
+python3 importance_classifier_production.py /scratch/c.scmag3/commonality_detection/output_files/ontology_completion_similar_thresh_50/wine
+
+echo 'Job Finished !!!'
