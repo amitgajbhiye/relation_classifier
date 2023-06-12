8470add7105e54d0a4037de20d31ca8b413f087c
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-06-12 14:14:42 +0100

    classification rel scoring babelnet domain

---
+++ run2_relscoring_classi_wordnet_all_static_emb.sh
@@ -0,0 +1,25 @@
+#!/bin/bash --login
+
+#SBATCH --job-name=FastTRelClass
+
+#SBATCH --output=logs/bablenet_domain/out_relscoring_classi_babelnet_domain_all_static_emb.txt
+#SBATCH --error=logs/bablenet_domain/err_relscoring_classi_babelnet_domain_all_static_emb.txt
+
+#SBATCH --tasks-per-node=5
+#SBATCH --ntasks=5
+#SBATCH -A scw1858
+
+#SBATCH -p gpu,gpu_v100
+#SBATCH --mem=20G
+
+#SBATCH -t 0-16:00:00
+
+#SBATCH --gres=gpu:2
+
+source venv_relbert/bin/activate
+
+module load CUDA/11.7
+
+python3 importance_classifier_production.py /scratch/c.scmag3/commonality_detection/output_files/classification_vocabs_similar_thresh_50/bablenet_domain
+
+echo 'Job Finished !!!'
